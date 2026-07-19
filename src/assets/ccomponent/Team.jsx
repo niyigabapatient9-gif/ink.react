470569099612440a04ecf1a862150e90691e95f4
iragena25egide <iragenaegide205@gmail.com>
2026-07-19 12:42:19 +0200

    change on return ang img

--- src/assets/ccomponent/Team.jsx
+++ src/assets/ccomponent/Team.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 import "./team.css";
 function Team(){
-    return(
-        <div class="team">
+    return (
+        <div className="team">
          <h1>Our team belives you deserve only the best.</h1>
          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-         <img src="teach.png"></img>
+         <img src="teach.png" alt="team" />
         </div>
     )
 }
