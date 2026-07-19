d7ed18d2b9a351fd80c6bb4b5a05c7614276ef41
iragena25egide <iragenaegide205@gmail.com>
2026-07-19 12:16:53 +0200

    team.css

---
+++ src/assets/ccomponent/Team.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./team.css";
+function team(){
+    return(
+        <div class="team">
+         <h1>Our team belives you deserve only the best.</h1>
+         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+        </div>
+    )
+}
+export default team;
