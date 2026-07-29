ddb98d5f600d2ac1e28504c53b01aedac047fde0
niyigaba patient <niyigabapatient9@gmail.com>
2026-07-29 11:27:15 -0700

    import slider

--- src/assets/ccomponent/Team.jsx
+++ src/assets/ccomponent/Team.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Slider from "./slider";
+
 
 import "./team.css";
 function Team(){
     return (
         <div className="team">
+            <Slider/>
          <h1>Our team belives you deserve only the best.</h1>
          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
          <img src="teach.png" alt="team" />
