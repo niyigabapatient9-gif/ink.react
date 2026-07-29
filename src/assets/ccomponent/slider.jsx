a06a5e0804bc4dc89516ccd0e67d042a9a02db14
niyigaba patient <niyigabapatient9@gmail.com>
2026-07-29 10:38:36 -0700

    tailwind and slider

--- src/assets/ccomponent/slider.jsx
+++ src/assets/ccomponent/slider.jsx
@@ -5,19 +5,18 @@ import "./slider.css";
 
 function slider(){
     return(
-        <div class="slider">
-            <div class="group">
-                <div class="card">1</div>
-                <div class="card">2</div>
-                <div class="card">3</div>
-                <div class="card">4</div>
+       <div className="corousel">
+        <div className="track">
+            <div className="item"><img src="black.png" alt="1"/>
+            <div className="item"><img src="africa.png" alt="2"/>
+            <div className="item"><img src="richone.png" alt="3"/>
+            <div className="item"><img src="teach.png" alt="4"/>
             </div>
-            <div  aria-hidden class="group">
-                <div class="card">1</div>
-                <div class="card">2</div>
-                <div class="card">3</div>
-                <div class="card">4</div>
+            </div>
+            </div>
             </div>
         </div>
+       </div>
+        
     )
 }
