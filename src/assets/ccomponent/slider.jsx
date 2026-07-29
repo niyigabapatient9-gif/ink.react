b0e2a5e7d45530a8227ff52dd9fb9cdaa5e0ba7b
niyigaba patient <niyigabapatient9@gmail.com>
2026-07-29 12:17:25 -0700

    sider change

--- src/assets/ccomponent/slider.jsx
+++ src/assets/ccomponent/slider.jsx
@@ -3,20 +3,24 @@
 import React from "react";
 import "./slider.css";
 
-function slider(){
-    return(
-       <div className="corousel">
-        <div className="track">
-            <div className="item"><img src="black.png" alt="1"/>
-            <div className="item"><img src="africa.png" alt="2"/>
-            <div className="item"><img src="richone.png" alt="3"/>
-            <div className="item"><img src="teach.png" alt="4"/>
-            </div>
-            </div>
-            </div>
-            </div>
-        </div>
-       </div>
-        
-    )
+function Slider() {
+  return (
+    <div className="corousel">
+      <div className="track">
+        {/* Isibo ya 1 y'amafoto */}
+        <div className="item"><img src="black.png" alt="1" /></div>
+        <div className="item"><img src="africa.png" alt="2" /></div>
+        <div className="item"><img src="richone.png" alt="3" /></div>
+        <div className="item"><img src="teach.png" alt="4" /></div>
+
+        {/* Isibo ya 2 (Duplicate) yo gutuma iguma kwizunguruka infinitly */}
+        <div className="item"><img src="black.png" alt="1" /></div>
+        <div className="item"><img src="africa.png" alt="2" /></div>
+        <div className="item"><img src="richone.png" alt="3" /></div>
+        <div className="item"><img src="teach.png" alt="4" /></div>
+      </div>
+    </div>
+  );
 }
+ 
+export default Slider;
