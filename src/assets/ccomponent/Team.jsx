d3f45f261cded1a840e36225a4f84635375da152
niyigaba patient <niyigabapatient9@gmail.com>
2026-07-31 06:26:04 -0700

    FQA import and in team

--- src/assets/ccomponent/Team.jsx
+++ src/assets/ccomponent/Team.jsx
@@ -1,24 +1,25 @@
+
 import React from "react";
 import Slider from "./slider";
-
-
 import "./team.css";
-function Team(){
-    return (
-        <div className="team">
-            <Slider/>
-         <h1>Our team belives you deserve only the best.</h1>
-         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-         <img src="teach.png" alt="team" />
-         <h3>Featured works.
-         </h3>
-         <section className="container">
-        <section className="images">
-         <img src ="bland.png"></img>
-         <img src="pink.png"></img>
-        </section>
-         </section>
-        </div>
-    )
+
+function Team() {
+  return (
+    <div className="team">
+      <Slider />
+      <section className="upper">
+        <h1>Our team belives you deserve only the best.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+        <img src="/teach.png" alt="team" />
+      </section>
+      <section className="down">
+        <h1>Featured works.</h1>
+          <img src="/bland.png" alt="bland" />
+          <img src="/pink.png" alt="pink" />
+        
+      </section>
+    </div>
+  );
 }
+
 export default Team;
